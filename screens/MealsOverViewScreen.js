b88c82c1a7ee03425bafef3cd3deb999b9763d55
leandre <tuyambazeleandre@gmail.com>
2024-01-25 21:35:46 +0200

    work on displaying meal items

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,11 +1,28 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Text } from 'react-native';
+// import { useRoute } from '@react-navigation/native'; // we can use this in case we don't need props
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 function MealsOverViewScreen({ route }) {
+  // const route = useRoute();
+  // const { categoryId } = route.params;
   const { categoryId } = route.params;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.includes(categoryId);
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.screen}>
-      <Text>Meal Overview Screen {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
