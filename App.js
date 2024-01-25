7aefd1dab0bebe7184edc30ad8b06222b18dce8c
leandre <tuyambazeleandre@gmail.com>
2024-01-25 20:22:37 +0200

    setup screen navigator

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen />
+        </Stack.Navigator>
+        <CategoriesScreen />
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
